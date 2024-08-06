5fb6ead7eb206dfea2e493e0262fac9ea44481ef
Abhinav Gera <abhinav.gera12@gmail.com>
2024-08-06 14:38:28 +0530

    no errors

--- migrations/env.py
+++ migrations/env.py
@@ -13,8 +13,9 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 config.set_section_option("devdb", "sqlalchemy.url", os.environ.get("DEV_DATABASE_URL"))
+config.set_section_option("testdb", "sqlalchemy.url", os.environ.get("TEST_DATABASE_URL"))
 
-target_metadata = models.Base.metadata 
+target_metadata = models.Base.metadata
 
 
 
--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,6 +1,18 @@
 import pytest
-from .utils.docker_utils import start_database_container
+from tests.utils.docker_utils import start_database_container
+from sqlalchemy import create_engine
+from tests.utils.database_utils import migrate_to_db
 
 @pytest.fixture(scope="session", autouse=True)
 def db_session():
     container = start_database_container()
+    
+    TEST_DATABASE_URL = "postgresql+psycopg2://postgres:postgres@127.0.0.1:5434/inventory"
+    engine = create_engine(TEST_DATABASE_URL)
+    
+    
+    with engine.begin() as connection:
+        migrate_to_db("migrations", "alembic.ini", connection)
+        
+    container.stop()
+    container.remove()
